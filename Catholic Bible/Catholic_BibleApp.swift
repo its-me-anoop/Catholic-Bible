07a28add3539093c9dad805e75cee74ebbb8f07c
Anoop Jose <anoop@flutterly.co.uk>
2024-05-17 10:52:08 +0100

    Initial Commit

---
+++ Catholic Bible/Catholic_BibleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Catholic_BibleApp.swift
+//  Catholic Bible
+//
+//  Created by Anoop Jose on 17/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Catholic_BibleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
